030c651de5a1313aa8336f77861a75556f455f7e
Joel Wembo <joelotepawembo@gmail.com>
2021-08-24 17:58:41 +0800

    Update app-routing.module.ts
    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [index];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
